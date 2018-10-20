4eb3a8589ebb8d23af899803437f2c30f6a542eb
oa4ajp <mendiola_loyola@yahoo.com>
2018-10-20 17:27:34 -0500

    Icon updated Version Updated.

--- AdifConverter/Properties/AssemblyInfo.cs
+++ AdifConverter/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdifConverter")]
+[assembly: AssemblyTitle("Adif Converter")]
 [assembly: AssemblyDescription("Adif Converter")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdifConverter")]
+[assembly: AssemblyProduct("Adif Converter")]
 [assembly: AssemblyCopyright("Copyright ©  2018, Alfredo Mendiola.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
